884fac6a4687ad8ff033f4bf1e4c8de912bbc7bc
prixone <prixone@users.noreply.github.com>
2017-05-11 14:14:57 -0300

    Added OAuthModel.php
    Added OAuthModel.php

---
+++ src/Models/OAuthModel.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * This Driver is based entirely on official documentation of the Mattermost Web
+ * Services API and you can extend it by following the directives of the documentation.
+ *
+ * God bless this mess.
+ *
+ * @author Luca Agnello <luca@gnello.com>
+ * @link https://api.mattermost.com/
+ */
+
+namespace Gnello\Mattermost\Models;
+
+/**
+ * Class OAuthModel
+ *
+ * @package Gnello\MattermostRestApi\Models
+ */
+class OAuthModel extends AbstractModel
+{
+    /**
+     * @var string
+     */
+    private static $endpoint = '/oauth';
+
+    /**
+     * @param array $requestOptions
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function registerOAuthApp(array $requestOptions)
+    {
+        return $this->client->post(self::$endpoint . '/apps', $requestOptions);
+    }
+
+    /**
+     * @param array $requestOptions
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getOAuthApps(array $requestOptions)
+    {
+        return $this->client->get(self::$endpoint . '/apps', $requestOptions);
+    }
+
+    /**
+     * @param $appId
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getOAuthApp($appId)
+    {
+        return $this->client->get(self::$endpoint . '/apps/' . $appId);
+    }
+
+    /**
+     * @param $appId
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function deleteOAuthApp($appId)
+    {
+        return $this->client->delete(self::$endpoint . '/apps/' . $appId);
+    }
+
+    /**
+     * @param $appId
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function regenerateOAuthAppSecret($appId)
+    {
+        return $this->client->post(self::$endpoint . '/apps/' . $appId . '/regen_secret');
+    }
+
+    /**
+     * @param $appId
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getOAuthAppInfo($appId)
+    {
+        return $this->client->get(self::$endpoint . '/apps/' . $appId . '/info');
+    }
+
+    /**
+     * @param $userId
+     * @param array $requestOptions
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getAuthorizedOAuthApps($userId, array $requestOptions)
+    {
+        return $this->client->get(UserModel::$endpoint . '/' . $userId . '/oauth/apps/authorized', $requestOptions);
+    }
+}
