12368c030264f0971c145deeb70b1e365f698e0f
prixone <prixone@users.noreply.github.com>
2017-05-13 19:25:01 -0300

    Update Driver.php
    Added missing Models.

--- src/Driver.php
+++ src/Driver.php
@@ -11,12 +11,21 @@
 
 namespace Gnello\Mattermost;
 
+use Gnello\Mattermost\Models\BrandModel;
 use Gnello\Mattermost\Models\ChannelModel;
+use Gnello\Mattermost\Models\ClusterModel;
+use Gnello\Mattermost\Models\CommandModel;
+use Gnello\Mattermost\Models\ComplianceModel;
 use Gnello\Mattermost\Models\FileModel;
+use Gnello\Mattermost\Models\LDAPModel;
+use Gnello\Mattermost\Models\OAuthModel;
 use Gnello\Mattermost\Models\PostModel;
 use Gnello\Mattermost\Models\PreferenceModel;
+use Gnello\Mattermost\Models\SAMLModel;
+use Gnello\Mattermost\Models\SystemModel;
 use Gnello\Mattermost\Models\TeamModel;
 use Gnello\Mattermost\Models\UserModel;
+use Gnello\Mattermost\Models\WebhookModel;
 use Pimple\Container;
 
 /**
@@ -33,7 +42,7 @@ class Driver
      */
     private $defaultOptions = [
         'scheme'    => 'https',
-        'basePath'  => '/api/v3',
+        'basePath'  => '/api/v4',
         'url'       => 'localhost',
         'login_id'  => null,
         'password'  => null,
@@ -72,18 +81,16 @@ class Driver
     public function authenticate()
     {
         $driverOptions = $this->container['driver'];
-        $options = [
+        $requestOptions = [
             'login_id' => $driverOptions['login_id'],
             'password' => $driverOptions['password']
         ];
 
-        /** @var Client $client */
-        $client = $this->container['client'];
-        $response = $client->post('/users/login', $options);
+        $response = $this->getUserModel()->loginToUserAccount($requestOptions);
 
         if ($response->getStatusCode() == 200) {
             $token = $response->getHeader('Token')[0];
-            $client->setToken($token);
+            $this->container['client']->setToken($token);
         }
 
         return $response;
@@ -114,26 +121,24 @@ class Driver
     }
 
     /**
-     * @param $teamId
      * @return ChannelModel
      */
-    public function getChannelModel($teamId)
+    public function getChannelModel()
     {
         if (!isset($this->models['channel'])) {
-            $this->models['channel'] = new ChannelModel($this->container['client'], $teamId);
+            $this->models['channel'] = new ChannelModel($this->container['client']);
         }
 
         return $this->models['channel'];
     }
 
     /**
-     * @param $teamId
      * @return PostModel
      */
-    public function getPostModel($teamId)
+    public function getPostModel()
     {
         if (!isset($this->models['post'])) {
-            $this->models['post'] = new PostModel($this->container['client'], $teamId);
+            $this->models['post'] = new PostModel($this->container['client']);
         }
 
         return $this->models['post'];
@@ -152,14 +157,123 @@ class Driver
     }
 
     /**
+     * @param $userId
      * @return PreferenceModel
      */
-    public function getPreferenceModel()
+    public function getPreferenceModel($userId)
     {
         if (!isset($this->models['preference'])) {
-            $this->models['preference'] = new PreferenceModel($this->container['client']);
+            $this->models['preference'] = new PreferenceModel($this->container['client'], $userId);
         }
 
         return $this->models['preference'];
+    }
+
+    /**
+     * @return WebhookModel
+     */
+    public function getWebhookModel()
+    {
+        if (!isset($this->models['webhook'])) {
+            $this->models['webhook'] = new WebhookModel($this->container['client']);
+        }
+
+        return $this->models['webhook'];
+    }
+
+    /**
+     * @return SystemModel
+     */
+    public function getSystemModel()
+    {
+        if (!isset($this->models['system'])) {
+            $this->models['system'] = new SystemModel($this->container['client']);
+        }
+
+        return $this->models['system'];
+    }
+
+    /**
+     * @return ComplianceModel
+     */
+    public function getComplianceModel()
+    {
+        if (!isset($this->models['compliance'])) {
+            $this->models['compliance'] = new ComplianceModel($this->container['client']);
+        }
+
+        return $this->models['compliance'];
+    }
+
+    /**
+     * @return CommandModel
+     */
+    public function getCommandModel()
+    {
+        if (!isset($this->models['command'])) {
+            $this->models['command'] = new CommandModel($this->container['client']);
+        }
+
+        return $this->models['command'];
+    }
+
+    /**
+     * @return ClusterModel
+     */
+    public function getClusterModel()
+    {
+        if (!isset($this->models['cluster'])) {
+            $this->models['cluster'] = new ClusterModel($this->container['client']);
+        }
+
+        return $this->models['cluster'];
+    }
+
+    /**
+     * @return BrandModel
+     */
+    public function getBrandModel()
+    {
+        if (!isset($this->models['brand'])) {
+            $this->models['brand'] = new BrandModel($this->container['client']);
+        }
+
+        return $this->models['brand'];
+    }
+
+    /**
+     * @return LDAPModel
+     */
+    public function getLDAPModel()
+    {
+        if (!isset($this->models['ldap'])) {
+            $this->models['ldap'] = new LDAPModel($this->container['client']);
+        }
+
+        return $this->models['ldap'];
+    }
+
+    /**
+     * @return OAuthModel
+     */
+    public function getOAuthModel()
+    {
+        if (!isset($this->models['oauth'])) {
+            $this->models['oauth'] = new OAuthModel($this->container['client']);
+        }
+
+        return $this->models['oauth'];
+    }
+
+    /**
+     * @return SAMLModel
+     */
+    public function getSAMLModel()
+    {
+        if (!isset($this->models['saml'])) {
+            $this->models['saml'] = new SAMLModel($this->container['client']);
+        }
+
+        return $this->models['saml'];
     }
 }
